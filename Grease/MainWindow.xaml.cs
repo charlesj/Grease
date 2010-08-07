bcd5ba4cca627c96295581fa5d1ced0c34a1b801
Josh Charles <josh.charles@gmail.com>
2010-08-06 19:45:45 -0500

    Directory is now saved between sessions.

--- Grease/MainWindow.xaml.cs
+++ Grease/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -24,12 +25,23 @@ namespace Grease
     {
         private MusicLibrary library;
         private Mp3Info currSong;
-
+        private UserSettingsGroup settings;
         public MainWindow()
         {
             InitializeComponent();
             library = new MusicLibrary();
 
+            var md = Settings.Default.MusicDirectory;
+            if (!string.IsNullOrEmpty(md) && md != "None")
+            {
+                LoadSongs(md);
+            }
+        }
+
+        private void LoadSongs(string folder)
+        {
+            library.Songs = FolderHelper.GetSongs(folder);
+            lblSongCount.Content = "Found " + library.Songs.Count.ToString() + " mp3's";
         }
 
         private void btnChooseDirectory_Click(object sender, RoutedEventArgs e)
@@ -37,19 +49,26 @@ namespace Grease
             var pathFinder = new FolderBrowserDialog();
             pathFinder.RootFolder = Environment.SpecialFolder.MyDocuments;
             var path = pathFinder.ShowDialog();
-            library.Songs = FolderHelper.GetSongs(pathFinder.SelectedPath);
-            lblSongCount.Content = "Found " + library.Songs.Count.ToString() + " mp3's";
+            if (!string.IsNullOrEmpty(pathFinder.SelectedPath))
+            {
+                LoadSongs(pathFinder.SelectedPath);
+                Settings.Default.MusicDirectory = pathFinder.SelectedPath;
+                Settings.Default.Save();
+            }
         }
 
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
-            if (currSong == null)
+            if (library.Songs != null && library.Songs.Count > 0)
             {
-                Player.Source = new Uri(library.GetRandomMp3().FullPath);
-                currSong = library.GetRandomMp3();
-                lblCurrentlyPlaying.Content = currSong.Name;
+                if (currSong == null)
+                {
+                    Player.Source = new Uri(library.GetRandomMp3().FullPath);
+                    currSong = library.GetRandomMp3();
+                    lblCurrentlyPlaying.Content = currSong.Name;
+                }
+                Player.Play();
             }
-            Player.Play();
         }
 
         private void btnPause_Click(object sender, RoutedEventArgs e)
@@ -60,10 +79,13 @@ namespace Grease
         private void btnNext_Click(object sender, RoutedEventArgs e)
         {
             Player.Pause();
-            currSong = library.GetRandomMp3();
-            Player.Source = new Uri(currSong.FullPath);
-            lblCurrentlyPlaying.Content = currSong.Name;
-            Player.Play();
+            if (library.Songs.Count > 0)
+            {
+                currSong = library.GetRandomMp3();
+                Player.Source = new Uri(currSong.FullPath);
+                lblCurrentlyPlaying.Content = currSong.Name;
+                Player.Play();
+            }
         }
 
         private void CompletedSong(object sender, RoutedEventArgs e)
